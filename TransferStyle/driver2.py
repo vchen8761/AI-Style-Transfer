d484218705375a6f43b0456b471e38cb2395047f
Victor Chen <vchen8761@gmail.com>
2020-04-17 14:23:09 -0400

    fixed windows specific figure bug

--- TransferStyle/driver2.py
+++ TransferStyle/driver2.py
@@ -27,35 +27,36 @@ import PIL
 from PIL import Image
 
 # https://github.com/Mckinsey666/Anime-Face-Dataset
-path_to_pics = "../../cropped/"
+# path_to_pics = "../../cropped/"
+path_to_pics = "../Anime-Face-Dataset/cropped/"
 
 content_image = ImageUtils.grab_image('https://gradschool.cornell.edu/wp-content/uploads/2018/07/JonPark.jpg')
 
 dirs = os.listdir(path_to_pics)
 shuffle(dirs)
 
+ImageUtils.enableflashplot()
+fig = plt.figure(figsize=(12,6))
 # For a whole directory
-for filename in dirs:
+for i, filename in enumerate(dirs):
+    if i > 5: break
     try: 
         style_image   = ImageUtils.grab_image(path_to_pics + filename)
         style_orig    = style_image
 
-        clip = ImageUtils.clip_0_1
+        style_image = ImageUtils.clip_0_1(content_image + style_image)
 
         hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
         stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+        #stylized_image = style_image
 
-        plt.figure(figsize=(12,6))
-        ImageUtils.enableflashplot()
+        fig.clf()
         plt.subplot(1, 3, 1); ImageUtils.imshow(content_image,  'Content Image')
         plt.subplot(1, 3, 2); ImageUtils.imshow(stylized_image, 'New Image')
-        plt.subplot(1, 3, 3); ImageUtils.imshow(style_image,    'Style Image')
+        plt.subplot(1, 3, 3); ImageUtils.imshow(style_orig,    'Style Image')
         ImageUtils.flashplot()
-
-        fig = plt.figure(1) 
-        fig.clf() 
-        ax = fig.subplots(nrows=2, ncols=1)
 
     except Exception as e:
         if input(f" - Image failed on {filename}: {e}.\n - Type q to quit or anything else to continue: ") == "q":
             break
+input('Finish? : ')
